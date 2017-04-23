ea447cd6648d5018a78362bb2c7bb2c255274ec4
Sajin Sabu <sajinsabu1905@gmail.com>
2017-04-23 00:07:47 -0400

    Lab 6 ex 5 Transmitter initialization done

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,16 +2,43 @@
 
 
 // LAB 6: Your driver code here
+struct e1000_tx_desc tx_desc_buf[TXRING_LEN] __attribute__ ((aligned (16)));
 
-int e1000_attach(struct pci_func *pcif)
+static void 
+e1000_init()
+{
+	int i;
+	for (i=0; i < TXRING_LEN; i++)
+	{
+		tx_desc_buf[i].buffer_addr = PADDR(&tx_desc_buf[i]);
+		tx_desc_buf[i].upper.fields.status = E1000_TXD_STAT_DD;
+	} 
+	mmio_e1000[E1000_TDBAL] = PADDR(tx_desc_buf);
+	mmio_e1000[E1000_TDLEN] = TXRING_LEN * sizeof(struct e1000_tx_desc);
+	mmio_e1000[E1000_TDBAH] = 0x0;
+	mmio_e1000[E1000_TDH] = 0x0;
+	mmio_e1000[E1000_TDT] = 0x0;
+	
+	mmio_e1000[E1000_TCTL] |= E1000_TCTL_EN;
+	mmio_e1000[E1000_TCTL] |= E1000_TCTL_PSP;
+	mmio_e1000[E1000_TCTL] |= E1000_TCTL_CT;	
+	mmio_e1000[E1000_TCTL] |= E1000_TCTL_COLD;	
+
+	mmio_e1000[E1000_TIPG] |= (E1000_TIPG_IPGT | E1000_TIPG_IPGR1 | E1000_TIPG_IPGR2);
+
+	
+	
+}
+
+
+int 
+e1000_attach(struct pci_func *pcif)
 {
 	pci_func_enable(pcif);
-	cprintf ("%x\n", pcif->reg_base[0]);
-	cprintf ("%x\n", pcif->reg_size[0]);
 	mmio_e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-	
-	cprintf("%x\n",mmio_e1000[E1000_STATUS]);
+
 	assert(mmio_e1000[E1000_STATUS] == 0x80080783);
+	e1000_init();
 	return 0;
 }
 	
